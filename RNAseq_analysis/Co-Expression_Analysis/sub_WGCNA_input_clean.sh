9cffa7c01468b12f77f574cb5158619243c8b863
Antonio Gómez Cortecero <antonio.gomez@emr.ac.uk>
2018-06-27 16:18:15 +0100

    Update

--- RNAseq_analysis/Co-Expression_Analysis/sub_WGCNA_input_clean.sh
+++ RNAseq_analysis/Co-Expression_Analysis/sub_WGCNA_input_clean.sh
@@ -5,6 +5,6 @@
 #$ -l h_vmem=8G
 #$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace11.blacklace
 
-scripts=/home/gomeza/git_repos/emr_repos/scripts/neonectria_ditissima/RNAseq_Analysis
+scripts=/home/gomeza/git_repos/emr_repos/scripts/neonectria_ditissima/RNAseq_analysis/Co-Expression_Analysis
 
 /home/adamst/prog/R/R-3.2.5/bin/Rscript --vanilla $scripts/WGCNA_input_clean.R --gene_table $1 --out_dir $2 --FPKM_start $3 --FPKM_end $4
