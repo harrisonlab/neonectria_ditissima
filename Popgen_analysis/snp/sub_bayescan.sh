3da1d2085ab9e627ae8ab3eea7953977be8e43b7
Antonio Gómez Cortecero <antonio.gomez@emr.ac.uk>
2018-07-31 11:42:57 +0100

    Bayescan 2.1 and plot

---
+++ Popgen_analysis/snp/sub_bayescan.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 20
+#$ -l virtual_free=1G
+#$ -l h=blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
+
+input=$1
+cpath=$PWD
+
+temp_dir="$TMPDIR"
+mkdir -p $temp_dir
+
+cp -r $input $temp_dir
+input_f=$(basename "$input")
+cd $temp_dir
+
+bayescan=/home/gomeza/bin/bayescan2.1/binaries/bayescan_2.1
+$bayescan -threads 16 -od ./ $input_f
+
+rm $input_f
+cp -r * $cpath
+rm -rf $temp_dir
+
+scripts=/home/gomeza/git_repos/emr_repos/scripts/neonectria_ditissima/Popgen_analysis/snp
+cd $cpath
+Rscript --vanilla $scripts/plot_bayescan.R ${input%vcf}_fst.txt
