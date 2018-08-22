1779f67cf702fb5c6c61e61468de20b06f63108a
Antonio Gómez Cortecero <antonio.gomez@emr.ac.uk>
2018-08-22 16:51:48 +0100

    Submit complete analysis of all possible sites

---
+++ Popgen_analysis/sub_complete.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 1
+#$ -l h_vmem=2G
+#$ -l h=blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
+
+input_dir=$1
+n=$2
+four_Ner_max=$3
+no_points=$4
+theta=$5
+
+cd $input_dir
+
+/home/gomeza/prog/LDhat-master/complete -n $n -rhomax $four_Ner_max -n_pts $no_points -theta $theta
